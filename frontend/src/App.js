daa039e3e0116fd194e67746535cceb1a88d4673
I-Tensei <tensei052581@gmail.com>
2025-07-09 17:27:43 +0900

    Resolve merge conflicts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [result, setResult] = useState(null);
 
   const handleCalc = async () => {
-    const res = await fetch('http://<EC2のパブリックDNS>:8080/calc', {
+    const res = await fetch('http://http://ec2-3-135-214-156.us-east-2.compute.amazonaws.com/:8080/calc', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ number: Number(input) }),
